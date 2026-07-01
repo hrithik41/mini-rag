4f2dd3f44aab9b4af54bfaeaa592dea4c02e334a
Hrithik-41 <littlehrithik9594@gmail.com>
2026-07-01 06:59:17 +0530

    Connected backend to mongodb atlas using mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
-
 const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
 
+app.use(cors());
 app.use(express.json());
+
+mongoose.connect(MONGO_URI)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.log("Error: ", err));
 
 app.get("/api/health", (req, res) => {
     res.json({
